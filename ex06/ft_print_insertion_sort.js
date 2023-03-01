16750dbd70220af03da42c6b1397f6d3738270be
GabrielToth <gabt71@gmail.com>
2023-03-01 19:43:03 +0100

    create index + start function

---
+++ ex06/ft_print_insertion_sort.js
@@ -0,0 +1,10 @@
+
+// Écrire une fonction qui prend en entrée un tableau d'entiers non trié et qui le trie par insertion. La fonction doit modiﬁer le tableau d'origine et afficher le tableau trié.
+
+
+
+function ft_print_insertion_sort(array) {
+ 
+  }
+
+ft_print_insertion_sort([2,6,9,4,2,3]);
