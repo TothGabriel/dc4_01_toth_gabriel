26fe542aa070e5245183a8c06b2fbb99578a989e
GabrielToth <gabt71@gmail.com>
2023-03-01 18:04:19 +0100

    create index + function

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,16 @@
+
+// Écrire une fonction qui affiche l’alphabet en majuscule, une string contenant l’alphabet en 
+// minuscule sera passée en paramètre la fonction. Chaque lettre devra être stockée dans une cellule 
+// du tableau.
+
+function displayAlphabet(string) {
+    alphabet = string.split('');
+    for (var i = 0; i < alphabet.length; i++) {
+        var codeascii = alphabet[i].charCodeAt(0) - 32;
+        var lettremaj = String.fromCharCode(codeascii);
+        alphabet[i] = lettremaj;
+        console.log(alphabet[i]);
+      }
+  }
+
+displayAlphabet('abcdefghijklmnopqrstuwxyz');
